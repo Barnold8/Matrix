3469148e9cc09afa0a8a5a0642a5a35b01584fb6
Brandon Wright <55092542+Barnold8@users.noreply.github.com>
2021-09-06 00:08:51 +0100

    Create functions.h

---
+++ functions.h
@@ -0,0 +1,16 @@
+#ifndef FUNCTIONS
+#define FUNCTIONS
+
+int nsleep(long miliseconds);
+
+int drawStuff(struct coords*,const int);
+
+int randomRange(const int, const int);
+
+void populate_array(struct coords* ,const int , const int);
+
+
+
+
+#endif
+
